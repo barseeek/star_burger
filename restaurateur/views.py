cbdf29d80882686486de4dc225b4cf2f9a69bc92
root <root@2649167-ay48707.twc1.net>
2024-04-27 13:47:47 +0300

    change requirements.txt

--- restaurateur/views.py
+++ restaurateur/views.py
@@ -95,7 +95,7 @@ def view_restaurants(request):
 
 
 @user_passes_test(is_manager, login_url='restaurateur:login')
-def view_orders(request):
+def view_orders(request): 
     items = []
     orders = (Order.objects.
               exclude(status__exact=Order.OrderStatus.DELIVERED).
