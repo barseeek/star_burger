e7578cdc7554ec9a12677b3d0d01325d72711887
root <root@3536141-ay48707.twc1.net>
2024-09-29 21:01:34 +0300

    fix script and docker compose file

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /opt/star_burger/
-docker compose down
+docker compose -f docker-compose.prod.yml down
 git pull
 docker compose -f docker-compose.prod.yml build
 docker compose -f docker-compose.prod.yml up -d
